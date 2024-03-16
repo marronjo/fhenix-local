0567514ab4ed8d8a972a4af07c3960bb8591d8d8
Eshel <eshel@scrtlabs.com>
2024-03-16 18:54:49 +0000

    deploy script using hardhat-deploy instead of ether's factory method

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -19,14 +19,44 @@ const func: DeployFunction = async function () {
     }
   }
 
-  const counter = await deploy("Counter", {
+  // const token0 = await tokenFactory.connect(contractOwner).deploy();
+  // const token1 = await tokenFactory.connect(contractOwner).deploy("Token1", "T1");
+  const token0 = await deploy("EncryptedToken", {
     from: signer.address,
-    args: [],
+    args: ["Token0", "T0"],
     log: true,
     skipIfAlreadyDeployed: false,
   });
 
-  console.log(`Counter contract: `, counter.address);
+  const token1 = await deploy("EncryptedToken", {
+    from: signer.address,
+    args: ["Token1", "T1"],
+    log: true,
+    skipIfAlreadyDeployed: false,
+  });
+
+  console.log("token 0 : " + token0);
+  console.log("token 1 : " + token1);
+
+  const token0Address = await token0.address;
+  const token1Address = await token1.address;
+
+  console.log("token 0 address : " + token0Address);
+  console.log("token 1 address : " + token1Address);
+
+  // const mysticDEX = await hre.ethers.getContractFactory("MysticDEX");
+  // const mysticdex = await mysticDEX.connect(contractOwner).deploy(token0Address, token1Address);
+  const mysticDEX = await deploy("MysticDEX", {
+    from: signer.address,
+    args: [token0Address, token1Address],
+    log: true,
+    skipIfAlreadyDeployed: false,
+  });
+
+  console.log("mystic dex : " + mysticDEX);
+  // await mysticdex.waitForDeployment();
+  // const address = await mysticdex.getAddress();
+  // return { mysticdex, address, token0, token1 };
 };
 
 export default func;
